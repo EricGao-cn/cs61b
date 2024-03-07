86d64458bb341d9d7ab641873266361f4e64b916
EricGao-cn <gaoyuxuan.cn@gmail.com>
2024-03-07 16:32:19 +0800

    DebugExercises finished

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -1,19 +1,17 @@
 /**
  * Exercise to showcase the step over button.
- * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
+ * Code adapted from <a href="https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java">...</a> and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. 
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        if(a > b){
+            return a;
+        }
+        else
+            return b;
     }
 
 
@@ -21,20 +19,7 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */    
     public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a+b;
     }
 
     /** Returns a new array where entry i is the max of
@@ -57,11 +42,9 @@ public class DebugExercise2 {
 
     /** Returns the sum of all elements in x. */
     public static int arraySum(int[] x) {
-        int i = 0;
         int sum = 0;
-        while (i < x.length) {
-            sum = sum + add(sum, x[i]);
-            i = i + 1;
+        for (int j : x) {
+            sum += j;
         }
         return sum;
     }
