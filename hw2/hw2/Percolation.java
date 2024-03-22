7427f422310d65f109ebe034974a987cefdeb3e7
EricGao-cn <gaoyuxuan.cn@gmail.com>
2024-03-22 23:58:28 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,69 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int length;
+    private int openNums;
+    private boolean[][] grid;
+    // disjointSet 下标n*n表示虚拟的顶， 下标n*n+1表示虚拟的底
+    private WeightedQuickUnionUF disjointSet;
+    public Percolation(int n) {
+        length = n;
+        openNums = 0;
+        grid = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = false;
+            }
+        }
+        disjointSet = new WeightedQuickUnionUF(n * n + 2);
+//        for (int i = 0; i < n; i++) {
+//            disjointSet.union(n * n, i);
+//            disjointSet.union(n * n + 1, n * n - i - 1);
+//        }
+    }
+
+    private int posToNum(int row, int col) {
+        return row * length + col;
+    }
+    private void connect(int row, int col) {
+        if (row == 0) {
+            disjointSet.union(length * length, col);
+        }
+        if (row == length - 1) {
+            disjointSet.union(length * length + 1, posToNum(row, col));
+        }
+        if (row - 1 >= 0 && grid[row-1][col]) {
+            disjointSet.union(posToNum(row - 1, col), posToNum(row, col));
+        }
+        if (row + 1 < length && grid[row+1][col]) {
+            disjointSet.union(posToNum(row + 1, col), posToNum(row, col));
+        }
+        if (col - 1 >= 0 && grid[row][col-1]) {
+            disjointSet.union(posToNum(row, col - 1), posToNum(row, col));
+        }
+        if (col + 1 < length && grid[row][col+1]) {
+            disjointSet.union(posToNum(row, col + 1), posToNum(row, col));
+        }
+    }
+
+    public void open(int row, int col) {
+        if (!grid[row][col]) {
+            grid[row][col] = true;
+            openNums++;
+            connect(row, col);
+        }
+    }
+    public boolean isOpen(int row, int col) {
+        return grid[row][col];
+    }
+    public boolean isFull(int row, int col) {
+        return disjointSet.connected(posToNum(row, col), length * length);
+    }
+    public int numberOfOpenSites() {
+        return openNums;
+    }
+    public boolean percolates() {
+        return disjointSet.connected(length * length, length * length + 1);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,61 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+
+import java.security.spec.PSSParameterSpec;
 
 public class PercolationStats {
+    private int[] result;
+    private int times;
 
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if (N * T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        result = new int[T];
+        times = T;
+        for (int i = 0; i < T; i++) {
+            Percolation perc = pf.make(N);
+            while (!perc.percolates()) {
+                int randomPos = StdRandom.uniform(N * N);
+                perc.open(randomPos / N, randomPos % N);
+            }
+            result[i] = perc.numberOfOpenSites();
+        }
+
+    }
+
+    public double mean() {
+        double sum = 0;
+        for (int i: result) {
+            sum += i;
+        }
+        return sum / times;
+    }
+
+    public double stddev() {
+        double sigmaSquare = 0;
+        double mean = mean();
+        for (int i: result) {
+            sigmaSquare += (i - mean) * (i - mean);
+        }
+        sigmaSquare /= (times - 1);
+        return Math.sqrt(sigmaSquare);
+    }
+
+    public double confidenceLow() {
+        double mu = mean();
+        double sigma = stddev();
+        return (mu - 1.96 * sigma / Math.sqrt(times));
+    }
+
+    public double confidenceHigh() {
+        double mu = mean();
+        double sigma = stddev();
+        return (mu + 1.96 * sigma / Math.sqrt(times));
+    }
+
+//    public static void main(String[] args) {
+//        PercolationStats perc = new PercolationStats(20, 100, new PercolationFactory());
+//        System.out.println(perc.mean());
+//    }
 }
