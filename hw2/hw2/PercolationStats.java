db9ef22da9b44ae55ae87ed931430adcf5030bd3
EricGao-cn <gaoyuxuan.cn@gmail.com>
2024-03-23 09:18:37 +0800

    hw2 finished

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.introcs.StdRandom;
 import java.security.spec.PSSParameterSpec;
 
 public class PercolationStats {
-    private int[] result;
+    private double[] result;
     private int times;
     private int length;
 
@@ -12,7 +12,7 @@ public class PercolationStats {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
-        result = new int[T];
+        result = new double[T];
         times = T;
         length = N;
         for (int i = 0; i < T; i++) {
@@ -21,24 +21,24 @@ public class PercolationStats {
                 int randomPos = StdRandom.uniform(N * N);
                 perc.open(randomPos / N, randomPos % N);
             }
-            result[i] = perc.numberOfOpenSites();
+            result[i] = (double) perc.numberOfOpenSites() / length / length;
         }
 
     }
 
     public double mean() {
         double sum = 0;
-        for (int i: result) {
+        for (double i: result) {
             sum += i;
         }
-        return sum / times / length / length;
+        return sum / times;
     }
 
     public double stddev() {
         double sigmaSquare = 0;
         double mean = mean();
-        for (int i: result) {
-            sigmaSquare += ((double) i / length - mean) * ((double) i / length - mean);
+        for (double i: result) {
+            sigmaSquare += (i / length) * (i / length);
         }
         sigmaSquare /= (times - 1);
         return Math.sqrt(sigmaSquare);
@@ -56,8 +56,11 @@ public class PercolationStats {
         return (mu + 1.96 * sigma / Math.sqrt(times)) * length * length;
     }
 
-    public static void main(String[] args) {
-        PercolationStats perc = new PercolationStats(20, 100, new PercolationFactory());
-        System.out.println(perc.mean());
-    }
+//    public static void main(String[] args) {
+//        PercolationStats perc = new PercolationStats(20, 100, new PercolationFactory());
+//        System.out.println(perc.mean());
+//        System.out.println(perc.stddev());
+//        System.out.println(perc.confidenceHigh());
+//        System.out.println(perc.confidenceLow());
+//    }
 }
